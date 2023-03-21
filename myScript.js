1f52669c8b1843a3eba930eb2e24023aba3957ce
Luukermanen <126602327+Luukermanen@users.noreply.github.com>
2023-03-21 14:47:03 +0100

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+import {customImages, test} from "./ripScript.js"
+
+customImages.push({
+    imageURL: 'https://pbs.twimg.com/profile_images/953600814920228864/shWeGhCN_400x400.jpg',
+    backgroundGradientColors: [[255, 0, 0], [0, 0, 255]] 
+})
+
+test()
